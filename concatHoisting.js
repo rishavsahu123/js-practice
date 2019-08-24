e48a592a2286a917f01241010010e7811449d61b
Rishav Sahu <rishavsahu123@gmail.com>
2019-08-24 15:08:38 +0530

    Create concatHoisting.js

---
+++ concatHoisting.js
@@ -0,0 +1,9 @@
+var temp = "rishav"
+// concat return updated data, it doesn't append in existing.
+temp.concat(' sahu')
+console.log(temp)
+
+arr = [1, 2, 3, 4]
+arr = arr.concat([3, 4, 4, 4])
+console.log(arr)
+var arr; //hoisting
