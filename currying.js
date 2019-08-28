4c15e4657263705d8114a39abc5f7b1830ec3bef
Rishav Sahu <rishavsahu123@gmail.com>
2019-08-28 09:01:02 +0530

    added dynamic function, but don't use this

--- currying.js
+++ currying.js
@@ -11,3 +11,16 @@ var sumES6 = a => b => c => a + b + c
 
 console.log(sumES5(1)(2)(3))
 console.log(sumES6(1)(2)(3))
+
+/*
+Dynamic but temp will be global variable and next time it will have the old value.
+var temp = 0
+var sum = (a) => {
+  if (!isNaN(a)) {
+    temp += a
+    return sum
+  }
+}
+sum(1)(2)(3)(2)
+console.log(temp)
+*/
