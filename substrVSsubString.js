7561e128631ffcca802371ceebc09b2c40a4f6de
Rishav Sahu <rishavsahu123@gmail.com>
2019-08-29 22:27:25 +0530

    Create substrVSsubString.js

---
+++ substrVSsubString.js
@@ -0,0 +1,11 @@
+var temp = "Hey, I am Rishav";
+temp = temp.substr(1,3) // beginning at the specified place, length of character
+console.log(temp)
+
+var temp1 = "Hey, I am Rishav";
+temp1 = temp1.substring(1,3) // beginning at the specified place, finish just before second param
+console.log(temp1)
+
+// output:
+// ey,
+// ey
